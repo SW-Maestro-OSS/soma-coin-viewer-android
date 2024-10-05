fe388aaf30147f793524d5b0c47d1ec776c387f2
tgyuuAn <atg0614@gmail.com>
2024-10-06 01:25:11 +0900

    [FEATURE] #7 - BottomNavigation 가시성 설정 로직 추가

--- presentation/src/main/java/com/soma/coinviewer/presentation/MainActivity.kt
+++ presentation/src/main/java/com/soma/coinviewer/presentation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.soma.coinviewer.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -12,16 +13,35 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
+    private val hideBottomNavDestinationIds: Set<Int> by lazy {
+        resources.obtainTypedArray(R.array.hideBottomNavDestinationIds).use { typedArray ->
+            (0 until typedArray.length())
+                .mapTo(mutableSetOf()) { index -> typedArray.getResourceId(index, 0) }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setNavigation()
+    }
+
+    private fun setNavigation() {
         binding.apply {
             val navHostFragment =
                 supportFragmentManager.findFragmentById(R.id.main_FCV) as NavHostFragment
             navController = navHostFragment.navController
             mainBNV.itemIconTintList = null
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.mainBNV.visibility = if (destination.id in hideBottomNavDestinationIds) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
         }
     }
 }
