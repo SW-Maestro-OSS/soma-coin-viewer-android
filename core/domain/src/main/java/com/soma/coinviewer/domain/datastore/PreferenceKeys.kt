f06cc703e02cdac2fef1c380e387c16e03f5473f
jung0115 <jungim7490@gmail.com>
2024-11-29 21:36:19 +0900

    [FEAT] #16 - DataStore key, value enum 세팅

---
+++ core/domain/src/main/java/com/soma/coinviewer/domain/datastore/PreferenceKeys.kt
@@ -0,0 +1,7 @@
+package com.soma.coinviewer.domain.datastore
+
+enum class PreferenceKeys(val key: String) {
+    PRICE_CURRENCY_UNIT("price_currency_unit"),
+    LANGUAGE("language"),
+    SHOW_SYMBOLS("show_symbols"),
+}
