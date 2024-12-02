6be163e5f971ecb79d984be87411b5a903f8ab44
jung0115 <jungim7490@gmail.com>
2024-12-02 19:53:51 +0900

    [FEAT] #16 - DataStore에 저장된 값에 따라 Switch 선택 보여주기

--- feature/setting/src/main/java/com/soma/coinviewer/feature/setting/SettingFragment.kt
+++ feature/setting/src/main/java/com/soma/coinviewer/feature/setting/SettingFragment.kt
@@ -1,13 +1,13 @@
 package com.soma.coinviewer.feature.setting
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.soma.coinviewer.common_ui.base.BaseBindingFragment
-import com.soma.coinviewer.domain.datastore.HowToShowSymbols
-import com.soma.coinviewer.domain.datastore.Language
 import com.soma.coinviewer.domain.datastore.PriceCurrencyUnit
+import com.soma.coinviewer.domain.datastore.Language
+import com.soma.coinviewer.domain.datastore.HowToShowSymbols
 import com.soma.coinviewer.feature.setting.databinding.FragmentSettingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,43 +18,57 @@ class SettingFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = fragmentViewModel
+
+        observeSelectType()
+
+        // 이전 선택 정보 불러오기
+        fragmentViewModel.getPriceCurrencyUnit()
+        fragmentViewModel.getLanguage()
+        fragmentViewModel.getHowToShowSymbols()
 
         // ✅ Switch Listener
         binding.switchPriceCurrency.setOnCheckedChangeListener { _, isChecked ->
-            binding.viewModel?.let { viewModel ->
-                val selectPriceCurrencyUnit = if (isChecked) {
-                    PriceCurrencyUnit.WON
-                } else {
-                    PriceCurrencyUnit.DEFAULT
-                }
-
-                viewModel.savePriceCurrencyUnit(selectPriceCurrencyUnit)
+            val selectPriceCurrencyUnit = if (isChecked) {
+                PriceCurrencyUnit.WON
+            } else {
+                PriceCurrencyUnit.DEFAULT
             }
+
+            fragmentViewModel.savePriceCurrencyUnit(selectPriceCurrencyUnit)
         }
 
         binding.switchLanguage.setOnCheckedChangeListener { _, isChecked ->
-            binding.viewModel?.let { viewModel ->
-                val selectLanguage = if (isChecked) {
-                    Language.KOREAN
-                } else {
-                    Language.DEFAULT
-                }
-
-                viewModel.saveLanguage(selectLanguage)
+            val selectLanguage = if (isChecked) {
+                Language.KOREAN
+            } else {
+                Language.DEFAULT
             }
+
+            fragmentViewModel.saveLanguage(selectLanguage)
         }
 
         binding.switchShowSymbolGrid.setOnCheckedChangeListener { _, isChecked ->
-            binding.viewModel?.let { viewModel ->
-                val selectHowToShowSymbols = if (isChecked) {
-                    HowToShowSymbols.Grid2x2
-                } else {
-                    HowToShowSymbols.DEFAULT
-                }
-
-                viewModel.saveHowToShowSymbols(selectHowToShowSymbols)
+            val selectHowToShowSymbols = if (isChecked) {
+                HowToShowSymbols.Grid2x2
+            } else {
+                HowToShowSymbols.DEFAULT
             }
+
+            fragmentViewModel.saveHowToShowSymbols(selectHowToShowSymbols)
         }
+    }
+
+    private fun observeSelectType() {
+        fragmentViewModel.priceCurrencyUnit.observe(viewLifecycleOwner, Observer { priceCurrencyUnit ->
+            binding.switchPriceCurrency.isChecked = (priceCurrencyUnit.value != PriceCurrencyUnit.DEFAULT.value)
+        })
+
+        fragmentViewModel.language.observe(viewLifecycleOwner, Observer { language ->
+            binding.switchLanguage.isChecked = (language.value != Language.DEFAULT.value)
+        })
+
+        fragmentViewModel.howToShowSymbols.observe(viewLifecycleOwner, Observer { howToShowSymbols ->
+            binding.switchShowSymbolGrid.isChecked = (howToShowSymbols.value != HowToShowSymbols.DEFAULT.value)
+        })
     }
 }
