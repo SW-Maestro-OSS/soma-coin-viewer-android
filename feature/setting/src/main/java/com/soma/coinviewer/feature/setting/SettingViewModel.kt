02eb5c2cededbc7109acc37697b353903944fd49
jung0115 <jungim7490@gmail.com>
2024-12-02 19:53:51 +0900

    [FEAT] #16 - DataStore에 저장된 값에 따라 Switch 선택 보여주기

--- feature/setting/src/main/java/com/soma/coinviewer/feature/setting/SettingViewModel.kt
+++ feature/setting/src/main/java/com/soma/coinviewer/feature/setting/SettingViewModel.kt
@@ -1,6 +1,5 @@
 package com.soma.coinviewer.feature.setting
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
