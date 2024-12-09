ed03dd4433bdbaf794d8505f0a40c05e28c362b0
jung0115 <jungim7490@gmail.com>
2024-12-09 22:00:50 +0900

    [REFACTOR] #16 - 데이터 저장, 조회 시 반복되는 부분을 확장 함수로 만들어서 사용

--- core/data/src/main/java/com/soma/coinviewer/data/datasource/LocalSettingDataSource.kt
+++ core/data/src/main/java/com/soma/coinviewer/data/datasource/LocalSettingDataSource.kt
@@ -14,44 +14,56 @@ import javax.inject.Inject
 class LocalSettingDataSource @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) {
-    // 데이터 저장
+    // ✅ 데이터 저장
     suspend fun savePriceCurrencyUnit(priceCurrencyUnit: PriceCurrencyUnit) {
-        dataStore.edit { preferences ->
-            preferences[PreferenceKeys.PRICE_CURRENCY_UNIT.key] = priceCurrencyUnit.value
-        }
+        dataStore.savePreference(PreferenceKeys.PRICE_CURRENCY_UNIT.key, priceCurrencyUnit) { it.value }
     }
 
     suspend fun saveLanguage(language: Language) {
-        dataStore.edit { preferences ->
-            preferences[PreferenceKeys.LANGUAGE.key] = language.value
-        }
+        dataStore.savePreference(PreferenceKeys.LANGUAGE.key, language) { it.value }
     }
 
     suspend fun saveHowToShowSymbols(howToShowSymbols: HowToShowSymbols) {
-        dataStore.edit { preferences ->
-            preferences[PreferenceKeys.HOW_TO_SHOW_SYMBOLS.key] = howToShowSymbols.value
-        }
+        dataStore.savePreference(PreferenceKeys.HOW_TO_SHOW_SYMBOLS.key, howToShowSymbols) { it.value }
     }
 
-    // 데이터 조회
+    // ✅ 데이터 조회
     fun getPriceCurrencyUnit(): Flow<PriceCurrencyUnit> {
-        return dataStore.data.map { preferences ->
-            val value = preferences[PreferenceKeys.PRICE_CURRENCY_UNIT.key]
+        return dataStore.getPreference(PreferenceKeys.PRICE_CURRENCY_UNIT.key) { value ->
             PriceCurrencyUnit.fromValue(value)
         }
     }
 
     fun getLanguage(): Flow<Language> {
-        return dataStore.data.map { preferences ->
-            val value = preferences[PreferenceKeys.LANGUAGE.key]
+        return dataStore.getPreference(PreferenceKeys.LANGUAGE.key) { value ->
             Language.fromValue(value)
         }
     }
 
     fun getHowToShowSymbols(): Flow<HowToShowSymbols> {
-        return dataStore.data.map { preferences ->
-            val value = preferences[PreferenceKeys.HOW_TO_SHOW_SYMBOLS.key]
+        return dataStore.getPreference(PreferenceKeys.HOW_TO_SHOW_SYMBOLS.key) { value ->
             HowToShowSymbols.fromValue(value)
         }
+    }
+}
+
+// ✅ 함수 재사용을 위한 확장 함수 정의
+private suspend fun <T> DataStore<Preferences>.savePreference(
+    key: Preferences.Key<String>,
+    value: T,
+    transform: (T) -> String
+) {
+    edit { preferences ->
+        preferences[key] = transform(value)
+    }
+}
+
+private fun <T> DataStore<Preferences>.getPreference(
+    key: Preferences.Key<String>,
+    transform: (String?) -> T
+): Flow<T> {
+    return data.map { preferences ->
+        val value = preferences[key]
+        transform(value)
     }
 }
